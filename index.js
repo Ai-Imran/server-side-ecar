6474522aba992a1c165aa4fe2c505ad0b0fad8ae
unknown <Ai-Imran>
2024-04-28 00:28:45 +0600

    add post users get and find

--- index.js
+++ index.js
@@ -30,6 +30,31 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+
+    const userCollection = client.db("Ismaqw").collection("users");
+
+    app.get('/users-my', async (req, res) => {
+      const users = await userCollection.find({}).toArray();
+      res.send(users); 
+  });
+
+  // POST a new user
+  app.post('/users-my', async (req, res) => {
+      const newUser = req.body;
+      const result = await userCollection.insertOne(newUser);
+      res.send(result); // Return the inserted user
+  });
+
+  // GET user by name
+  app.get('/users-my/:name', async (req, res) => {
+      const userName = req.params.name;
+      const user = await userCollection.findOne({ name: userName });
+      res.send(user); 
+  });
+  
+  
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
