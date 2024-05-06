7ee81db8669dd88ac1ff0fa70f189bf816e82a54
unknown <Ai-Imran>
2024-05-07 02:56:11 +0600

    adding allorders

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -35,6 +36,33 @@ async function run() {
     const orderCollection = client.db("Ismaqw").collection("orders");
     const problemsCollection = client.db("Ismaqw").collection("problems");
     const userPostCollection = client.db("Ismaqw").collection("posts");
+
+    //  const verifyToken = (req, res, next) => {
+    //   // console.log('inside verify token', req.headers.authorization);
+    //   if (!req.headers.authorization) {
+    //     return res.status(401).send({ message: 'unauthorized access' });
+    //   }
+    //   const token = req.headers.authorization.split(' ')[1];
+    //   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    //     if (err) {
+    //       return res.status(401).send({ message: 'unauthorized access' })
+    //     }
+    //     req.decoded = decoded;
+    //     next();
+    //   })
+    // }
+
+    // // use verify admin after verifyToken
+    // const verifyAdmin = async (req, res, next) => {
+    //   const email = req.decoded.email;
+    //   const query = { email: email };
+    //   const user = await userCollection.findOne(query);
+    //   const isAdmin = user?.role === 'admin';
+    //   if (!isAdmin) {
+    //     return res.status(403).send({ message: 'forbidden access' });
+    //   }
+    //   next();
+    // }
 
     app.get('/users-my', async (req, res) => {
       const users = await userCollection.find({}).sort({ timestamp: -1 }).toArray();
